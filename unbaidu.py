9dd4f893ac59f7499238f10f01e85aebfb78ab98
LeiYangGH <675686066@qq.com>
2016-12-05 18:00:49 +0800

    can find by finish time

--- unbaidu.py
+++ unbaidu.py
@@ -10,23 +10,25 @@ from datetime import datetime
 
 open_url = r'http://zhidao.baidu.com/question/%s.html?entry=qb_ihome_tag' 
 
-days = 10 
+days = int(input('days?')) 
+print(days)
 
 def diff_days(date):
     #if date.startswith(b'\xe4\xbb\x8a\xe5\xa4\xa9') or date.endswith(b'\xe5\x89\x8d'): 
     if date.startswith('今天') or date.endswith('前'): 
         return 0
-    last_date = datetime.strptime(date, '%Y-%m-%d %H:%M')
-    print(last_date)
+    #last_date = datetime.strptime(date, '%Y-%m-%d %H:%M')
+    last_date = datetime.strptime(date, '%Y-%m-%d')
+    #print(last_date)
     return (datetime.now() - last_date).days
 
 def urlopen(page):
-    print('page=' + str(page))
+    print('\npage=' + str(page))
     #time.sleep(2)
     timestamp = int(round(time.time() * 1000))
-    print(timestamp)
+    #print(timestamp)
     url = r'https://zhidao.baidu.com/ihome/api/myanswer'
-    size = 12
+    size = 50 
     parms = {
     'pn' : (page - 1) * size,
     'rn' : size,
@@ -41,8 +43,8 @@ def urlopen(page):
         #r = requests.get(url, data=parms, cookies=headers)
         r = requests.get(url, params=parms, cookies=headers)
         t = r.text
-        print(t)
-        print(r.url)
+        #print(t)
+        #print(r.url)
         return t
     except Exception as e:
         print(e)
@@ -68,14 +70,15 @@ def baidu_answer_nobest():
         items = objs['data']['question']['list']
         # display list
         for i in items:
-            item_days = diff_days(i['createTime'])
+            item_days = diff_days(i['finishTime'])
             #item_days = diff_days(i['createTime'].encode('utf8')) 
             #if x > 10: return urls
-            print("d="+ str(item_days))
-            if item_days > days: return urls
+            print(str(item_days), end='\t')
+            if item_days > int(days): return urls
 
            # if i['qStatus'] is 2:
-            if i['qStatus'] is 2 and i['isBest'] == '0':
+            if i['qStatus'] is 2 and i['isBest'] == '0' and i['isRecommended'] != 1:
+                print(i['qid'])
                 urls.append(open_url % i['qid'])
         #x = x + 1
         #page+=1
@@ -84,5 +87,7 @@ def baidu_answer_nobest():
 urls = baidu_answer_nobest()
 sys.stdout.write('\n')
 
-if input('Are you sure open %d urls? [Y/n] ' % len(urls)) == 'y':
+if len(urls) < 10:
     [urlcall(url) for url in urls]
+else:
+    print(urls)
