0600975d92b4be5bf1dacfc93c7220f17bfb99d8
LeiYangGH <675686066@qq.com>
2017-08-10 17:34:21 +0800

    can get face location

--- facerecogition/face.py
+++ facerecogition/face.py
@@ -1,4 +1,4 @@
 import face_recognition
-image = face_recognition.load_image_file("obama.png")
+image = face_recognition.load_image_file("obamaz.jpg")
 face_locations = face_recognition.face_locations(image)
 print(face_locations) 
