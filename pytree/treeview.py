f06ae4fe1d91d44fc3865d0fd3e5dcb949e7e506
LeiYangGH <675686066@qq.com>
2017-08-23 21:51:58 +0800

    remove dynamic input nodes

--- pytree/treeview.py
+++ pytree/treeview.py
@@ -7,7 +7,6 @@ ported to Python 3.1 for the 4th edition of this book, June 2010;
 from tkinter import *
 from treeview_wrappers import TreeWrapper, TreeViewer
 import btree
-#from parser2 import * 
 import parser2
      
 ###################################################################
@@ -75,26 +74,16 @@ class ParseTreeWrapper(TreeWrapper):
         except:
             return 'Value = <error>'
 
-    def onInputLine(self, line, viewer):            # on input line
-        p = parser2.Parser()                        # parse expr text
-        p.lex.newtext(line)                         # draw resulting tree
-        t = p.analyse()
-        if t: viewer.drawTree(t)
      
 ###################################################################
 # canned test cases (or type new nodelists/exprs in input field)
 ###################################################################
      
-def shownodes(sequence):
-    sequence = map(str, sequence)           # convert nodes to strings
-    entry.delete(0, END)                    # show nodes in text field
-    entry.insert(0, ' '.join(sequence))     # 3.X: sequence is a generator
      
 def test1_binary():                         # tree type is binary wrapper
     nodes = [3, 1, 9, 2, 7]                 # make a binary tree
     tree  = BinaryTree(viewer)              # embed viewer in tree
     for i in nodes: tree.insert(i)            
-    shownodes(nodes)                        # show nodes in input field
     tree.view()                             # sketch tree via embedded viewer
      
      
@@ -114,15 +103,7 @@ if __name__ == '__main__':
     var = StringVar()
     var.set('btree')
     
-    def onInputLine():
-        line = entry.get()              # use per current tree wrapper type
-        viewer.onInputLine(line)        # type a node list or expression
      
-    Button(root, text='input', command=onInputLine).pack(side=RIGHT)
-    entry = Entry(root)
-    entry.pack(side=RIGHT, expand=YES, fill=X)
-    entry.bind('<Return>', lambda event: onInputLine())   # button or enter key
     test1_binary()                          
-    #root.geometry("1080x800+100+100") 
     root.geometry("600x600+100+100") 
     root.mainloop()                                       # start up the gui
