faa1c95f63cdcb270ae4b23ffcc99bbb945cf77c
Lei, Yang <yang_lei@mcafee.com>
2017-08-24 14:04:10 +0800

    draw in constructor

--- pytree/treeview.py
+++ pytree/treeview.py
@@ -4,12 +4,11 @@ import btree
      
 root = Tk()                             # build a single viewer gui
 bwrapper = TreeWrapper()          # add extras: input line, test btns
-viewer   = TreeViewer(bwrapper, root)   # start out in binary mode
 
 nodes = [3, 1, 9, 2, 7]                 # make a binary tree
 tree  = btree.BinaryTree()             # embed viewer in tree
 for i in nodes: tree.insert(i)            
-viewer.drawTree(tree.tree)
+TreeViewer(bwrapper, root,tree.tree)   # start out in binary mode
      
                          
 root.mainloop()                                       # start up the gui
