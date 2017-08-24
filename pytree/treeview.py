5544e6ae1d324999afaaa4a397ca4b7d52a401a3
Lei, Yang <yang_lei@mcafee.com>
2017-08-24 11:41:28 +0800

    remove one tree view wrapper

--- pytree/treeview.py
+++ pytree/treeview.py
@@ -15,24 +15,14 @@ class BinaryTreeWrapper(TreeWrapper):          # embed binary tree in viewer
         except: 
             return str(node)
 
-     
-class BinaryTree(btree.BinaryTree):
-    def __init__(self, viewer):                # embed viewer in tree
-        btree.BinaryTree.__init__(self)        # but viewer has a wrapper
-        self.viewer  = viewer
-
-    def view(self):
-        self.viewer.drawTree(self.tree)
-
-
 root = Tk()                             # build a single viewer gui
 bwrapper = BinaryTreeWrapper()          # add extras: input line, test btns
 viewer   = TreeViewer(bwrapper, root)   # start out in binary mode
 
 nodes = [3, 1, 9, 2, 7]                 # make a binary tree
-tree  = BinaryTree(viewer)              # embed viewer in tree
+tree  = btree.BinaryTree()             # embed viewer in tree
 for i in nodes: tree.insert(i)            
-tree.view()         
+viewer.drawTree(tree.tree)
      
                          
 root.mainloop()                                       # start up the gui
