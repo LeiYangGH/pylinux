7b0ef85ad8aa79b147e4de51bcb3903a1495f282
Lei, Yang <yang_lei@mcafee.com>
2017-09-12 20:03:38 +0800

    dict

--- win/CombileDailyLogs/main.py
+++ win/CombileDailyLogs/main.py
@@ -3,23 +3,24 @@ import re
 import os
 
 def get_all_logs(dir):
-    f = []
+    f = {}
     for (dirpath, dirnames, filenames) in walk(dir):
-        logs = [(fn,os.path.join(dirpath, fn)) for fn in filenames if re.match(r'\d{4}-\d{2}-\d{2}.txt$', fn) is not None]
-        f.extend(logs)
-    f.sort(key=lambda x:x[0])
+        logs = {fn:os.path.join(dirpath, fn) for fn in filenames if re.match(r'\d{4}-\d{2}-\d{2}.txt$', fn) is not None}
+        f.update(logs)
+    #f.sort(key=lambda x:x.key)
     return f
 
 def join_logs(logs,allfile):
     with open(allfile,'wt',encoding='utf-8') as all:
-         for log in logs:
-             fullname = log[1]
+         for (k,v) in logs.items():
+             fullname = v
              try:
                  print(fullname)
                  logf = open(fullname, 'r',encoding='utf-8')
                  content = logf.read()
-                 all.write("="*20+log[0]+"="*20+"\r\n"*2)
+                 all.write("="*20+k+"="*20+"\r\n"*2+"\n")
                  all.writelines(content)
+                 all.write( "\r\n" * 2)
              except UnicodeDecodeError:
                  print(fullname)
                  pass
